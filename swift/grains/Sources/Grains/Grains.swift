3b5726b7a848dda04ca7bd7d0b704bc04b196639
Tim Jarratt <tjarratt@pivotal.io>
2020-08-10 17:01:10 +0200

    Add solution for Grains

---
+++ swift/grains/Sources/Grains/Grains.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+struct Grains {
+
+  static let total: UInt = 18_446_744_073_709_551_615
+
+  static func square(_ number: Int) throws -> UInt {
+    guard number >= 1 else {
+      throw GrainsError.inputTooLow(errorMessageFor(input: number))
+    }
+    guard number <= 64 else {
+      throw GrainsError.inputTooHigh(errorMessageFor(input: number))
+    }
+
+    let firstValue = UInt(1)
+    return (1..<number).reduce(firstValue) { acc, _ in acc * 2 }
+  }
+
+  enum GrainsError: Error {
+    case inputTooLow(String)
+    case inputTooHigh(String)
+  }
+
+  private static let tooLowError = GrainsError.inputTooLow("tata")
+  private static let tooHighError = GrainsError.inputTooHigh("toto")
+
+  private static func errorMessageFor(input: Int) -> String {
+    return "Input[\(input)] invalid. Input should be between 1 and 64 (inclusive)"
+  }
+}
