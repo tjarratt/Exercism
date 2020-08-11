30df9c20f980572a6b4c687066b7aecee1aaef61
Tim Jarratt <tjarratt@pivotal.io>
2020-08-11 11:56:11 +0200

    More compact solution, with less nil checking

--- swift/hamming/Sources/Hamming/Hamming.swift
+++ swift/hamming/Sources/Hamming/Hamming.swift
@@ -2,11 +2,8 @@ struct Hamming {
   static func compute(_ strand: String, against otherStrand: String) -> Int? {
     guard strand.count == otherStrand.count else { return nil }
 
-    return zip(Array(strand), Array(otherStrand)).reduce(0) { acc, arr in
-      guard let acc = acc else { return 0 }
-
-      if arr.0 != arr.1 { return acc + 1 }
-      else { return acc }
-    }
+    return zip(Array(strand), Array(otherStrand))
+      .compactMap { this, that in this != that ? 1 : nil }
+      .reduce(0, +)
   }
 }
